921c2a04c2b30e9b035d9a702ef6ff67b93e88d3
Francois Martel <fmrtl73@gmail.com>
2019-04-20 19:46:27 +0000

    automation

--- postgres/k8s/benchmarking/clean-all.sh
+++ postgres/k8s/benchmarking/clean-all.sh
@@ -1,3 +1,10 @@
 for sc in $(kubectl get sc | grep -v NAME | grep -v stork | awk '{print $1}')
- do kubectl delete -f $sc.yaml
+ do 
+    kubectl delete -f $sc.yaml
+    init=`echo $sc | sed 's/psql-/pgbench-init-/g'`
+    echo $init
+    run=`echo $sc | sed 's/psql-/pgbench-run-/g'`
+    echo $run
+    kubectl delete -f $init.yaml
+    kubectl delete -f $run.yaml
 done
