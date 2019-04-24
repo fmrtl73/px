a10c18eda223913c51453a163f5a54bd9a9f8f9d
Francois Martel <fmrtl73@gmail.com>
2019-04-24 15:51:40 +0000

    improved logging

--- postgres/k8s/benchmarking/run.sh
+++ postgres/k8s/benchmarking/run.sh
@@ -8,48 +8,43 @@ bench(){
   cp pgbench-run-template.yaml $sc/pgbench-run.yaml
   svcname=`echo $sc | awk '{ print toupper($0) }' | sed 's/-/_/g'`
   svcname=`echo $svcname`_SVC
-  echo $svcname
   sed -i "s/SC_NAME/$sc/g" $sc/*.yaml
   sed -i "s/SVC_NAME/$svcname/g" $sc/*.yaml
 
-  echo "Deploying psql for storage class $sc"
-  kubectl apply -f $sc/psql.yaml
-  echo "Waiting for $podname pod to start, will timeout after 2 minutes"
+  echo "##### Deploying psql test for storage class $sc"
+  kubectl apply -f $sc/psql.yaml | awk '{print " # " $0}'
+  echo " # Waiting for $podname pod to start, will timeout after 2 minutes"
   pod=`kubectl get pod -n portworx | grep $sc-app | awk '{print $1}'`
-  echo $pod
-  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m
+  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m | awk '{print " # " $0}'
   if (( $? )); then
     echo "Failure" >&2
     exit 1
   fi
-  echo "Deploying pgbench init for storage class $sc"
-  kubectl create -f $sc/pgbench-init.yaml
-  echo "waiting for pgbench init pod to be ready, will timeout after 2 minutes"
+  kubectl create -f $sc/pgbench-init.yaml | awk '{print " # " $0}'
+  echo " # Waiting for pgbench init pod to be ready, will timeout after 2 minutes"
   pod=`kubectl get pod -n portworx | grep pgbench-init-$sc | awk '{print $1}'`
-  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m
+  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m | awk '{print " # " $0}'
   if (( $? )); then
     echo "Failure" >&2
     exit 1
   fi
-  echo "waiting for $init to complete, will timeout after 5 minutes"
-  kubectl -n portworx wait --for=condition=complete job/pgbench-init-$sc --timeout 5m
+  echo " # Waiting for init pod to complete, will timeout after 5 minutes"
+  kubectl -n portworx wait --for=condition=complete job/pgbench-init-$sc --timeout 5m | awk '{print " # " $0}'
   if (( $? )); then
     echo "Failure" >&2
     exit 1
   fi
-  echo "Deploying pgbench run for storage class $sc"
-  kubectl create -f $sc/pgbench-run.yaml
-  echo "waiting for $run pod to be ready, will timeout after 2 minutes"
+  kubectl create -f $sc/pgbench-run.yaml | awk '{print " # " $0}'
+  echo " # Waiting for pgbench-run pod to be ready, will timeout after 2 minutes"
   pod=`kubectl get pod -n portworx | grep pgbench-run-$sc | awk '{print $1}'`
-  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m
+  kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m | awk '{print " # " $0}'
   if (( $? )); then
     echo "Failure" >&2
     exit 1
   fi
-  echo "Pod ready, tailing logs"
-  echo "########## $run results ###############"
-  kubectl -n portworx logs -f $pod
-  echo "########## $run results end ###############"
+  echo " # Results for storage classe: $sc "
+  kubectl -n portworx logs -f $pod | awk '{print "     " $0}'
+  echo "        "
 }
 display_usage(){
   echo "This script requires parameters to run"
