f38f3c3fb4ed66aed2b462d7a447837b0b5e8db2
Francois Martel <fmrtl73@gmail.com>
2019-06-18 18:51:07 +0000

    i++

--- nifi/k8s/deploy-nifi.sh
+++ nifi/k8s/deploy-nifi.sh
@@ -10,5 +10,11 @@ echo "Building for environment $ENV"
 rm -rf apache-nifi/deployment || true
 
 vortex --template apache-nifi/templates --output apache-nifi/deployment -varpath ./apache-nifi/environments/$ENV.yaml
-
+kubectl create ns nifi
 kubectl create -f apache-nifi/deployment/
+kubectl -n nifi wait --for=condition=Ready po/nifi-0 --timeout 2m | awk '{print " # " $0}'
+  if (( $? )); then
+    echo "Failure" >&2
+    exit 1
+  fi
+kubectl cp jars nifi-0:/opt/nifi/lib -n nifi
