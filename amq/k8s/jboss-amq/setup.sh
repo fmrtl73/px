12b29436d103bda9beeef43b0c569d8f872069c3
Francois Martel <francoismartel@Francoiss-iPro.fios-router.home>
2019-09-21 15:13:23 -0400

    adding jboss-amq

---
+++ amq/k8s/jboss-amq/setup.sh
@@ -0,0 +1,9 @@
+kubectl create -f sc.yaml
+kubectl apply -f amq.yaml
+pod=`kubectl get po | grep amq | awk '{print $1}'`
+kubectl -n portworx wait --for=condition=Ready po/$pod --timeout 2m | awk '{print " # " $0}'
+if (( $? )); then
+  echo "Failure" >&2
+  exit 1
+fi
+kubectl cp amq-util $pod:/tmp/utility
