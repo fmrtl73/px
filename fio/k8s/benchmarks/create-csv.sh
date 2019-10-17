eb87822456804997c55a8473d37245f6db9fe2b1
Francois Martel <fmrtl73@gmail.com>
2019-10-17 17:46:34 +0000

    i++

--- fio/k8s/benchmarks/create-csv.sh
+++ fio/k8s/benchmarks/create-csv.sh
@@ -1,4 +1,11 @@
-POD=`kubectl -n portworx get po -l job-name=fio-job-fio | grep -v NAME | awk '{print $1}'`
+sc=$1   # first command line argument
+if [ $# -eq 0 ]; then
+    echo "please enter the storage class name as a parameter, example: create-csv.sh fio-repl1"
+    exit 1
+fi
+
+
+POD=`kubectl -n portworx get po -l job-name=fio-job-$1 | grep -v NAME | awk '{print $1}'`
 kubectl -n portworx logs $POD > fio-results.json
 jq '.jobs[] | select(.jobname | contains("ephemeral"))  | {jobname, groupid, read_iops: .read.iops, write_iops: .write.iops, read_lat: .read.lat.mean, write_lat: .write.lat.mean, read_bw: .read.bw, write_bw: .write.bw }' fio-results.json | jq -r ' flatten | @csv' > /vagrant/fio-results.csv
 jq '.jobs[] | select(.jobname | contains("portworx"))  | {jobname, groupid, read_iops: .read.iops, write_iops: .write.iops, read_lat: .read.lat.mean, write_lat: .write.lat.mean, read_bw: .read.bw, write_bw: .write.bw }' fio-results.json | jq -r ' flatten | @csv' >> /vagrant/fio-results.csv
