1cbb85f721bab606fe6b85a1089946a2814ffa17
fmrtl73 <33674533+fmrtl73@users.noreply.github.com>
2019-10-18 12:57:12 -0400

    Update create-csv.sh

--- fio/k8s/benchmarks/create-csv.sh
+++ fio/k8s/benchmarks/create-csv.sh
@@ -4,8 +4,14 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-
+# get the pod name
 POD=`kubectl -n portworx get po -l job-name=fio-job-$1 | grep -v NAME | awk '{print $1}'`
+
+# output logs to fio-results.json
 kubectl -n portworx logs $POD > fio-results.json
+
+# use jq to get iops/lat/bdwth for all ephemeral jobs and write to csv
 jq '.jobs[] | select(.jobname | contains("ephemeral"))  | {jobname, groupid, read_iops: .read.iops, write_iops: .write.iops, read_lat: .read.lat.mean, write_lat: .write.lat.mean, read_bw: .read.bw, write_bw: .write.bw }' fio-results.json | jq -r ' flatten | @csv' > fio-results.csv
+
+# use jq to get iops/lat/bdwth for all portworx jobs and append to csv file
 jq '.jobs[] | select(.jobname | contains("portworx"))  | {jobname, groupid, read_iops: .read.iops, write_iops: .write.iops, read_lat: .read.lat.mean, write_lat: .write.lat.mean, read_bw: .read.bw, write_bw: .write.bw }' fio-results.json | jq -r ' flatten | @csv' >> fio-results.csv
